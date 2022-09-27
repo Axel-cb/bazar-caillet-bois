b8d52ddf1bfe3c0ade3a706791042136a3c92c31
Axel-cb <axel.cbois91@gmail.com>
2022-09-26 23:21:05 -0300

    entrega proyecto final

--- js/app.js
+++ js/app.js
@@ -1,40 +1,11 @@
-/*   carrito de compras con localStorage y JSON en la pagina de "nuestros productos"     */
-
-const stock = [
-    { id: 1, imagen: "../assets-bazar/imagenes-mejoradas/acero-blanco.avif", nombre: "Termos de Acero Luminox en Colores – Blanco", precio: 10152, },
-    { id: 2, imagen: "../assets-bazar/imagenes-mejoradas/acero-negro.avif", nombre: "Termo de Acero Luminox Negro", precio: 10152, },
-    { id: 3, imagen: "../assets-bazar/imagenes-mejoradas/acero-boca.avif", nombre: "Termo de Acero Luminox Boca Jrs – Camiseta", precio: 10295, },
-    { id: 4, imagen: "../assets-bazar/imagenes-mejoradas/acero-arg.avif", nombre: "Termo de Acero Luminox Bandera Argentina", precio: 10152, },
-    { id: 5, imagen: "../assets-bazar/imagenes-mejoradas/tangoazul1.webp", nombre: "Termo Tango 1000 Compacto – Azul", precio: 3177, },
-    { id: 6, imagen: "../assets-bazar/imagenes-mejoradas/tangorosa1.webp", nombre: "Termo Tango 1000 Compacto – Rosa", precio: 3177, },
-    { id: 7, imagen: "../assets-bazar/imagenes-mejoradas/sigmaazul1.webp", nombre: "Termo Sigma – Azul", precio: 2463, },
-    { id: 8, imagen: "../assets-bazar/imagenes-mejoradas/sigmavioleta1.webp", nombre: "Termo Sigma – Violeta", precio: 2463, },
-    { id: 9, imagen: "../assets-bazar/imagenes-mejoradas/alfarojo.webp", nombre: "Termo Alfa – Rojo", precio: 2321, },
-    { id: 10, imagen: "../assets-bazar/imagenes-mejoradas/alfagris.avif", nombre: "Termo Alfa – Gris", precio: 2321, },
-    { id: 11, imagen: "../assets-bazar/imagenes-mejoradas/tatuazul.webp", nombre: "Termo Tatú – Azul", precio: 2215, },
-    { id: 12, imagen: "../assets-bazar/imagenes-mejoradas/taturojo1.webp", nombre: "Termo Tatú – Rojo", precio: 2215, },
-    { id: 13, imagen: "../assets-bazar/imagenes-mejoradas/boca1.webp", nombre: "Termo Mate Joven Boca Jrs", precio: 2215, },
-    { id: 14, imagen: "../assets-bazar/imagenes-mejoradas/river1.webp", nombre: "Termo Mate Joven River Plate", precio: 2215, },
-    { id: 15, imagen: "../assets-bazar/imagenes-mejoradas/casla1.webp", nombre: "Termo Mate Joven San Lorenzo", precio: 2215, },
-    { id: 16, imagen: "../assets-bazar/imagenes-mejoradas/racing1.webp", nombre: "Termo Mate Joven Racing Club", precio: 2215, },
-    { id: 17, imagen: "../assets-bazar/imagenes-mejoradas/cai1.webp", nombre: "Termo Mate Joven Independiente", precio: 2215, },
-    { id: 18, imagen: "../assets-bazar/imagenes-mejoradas/capriceblanco1.webp", nombre: "Jarra Caprice – Blanco", precio: 3026, },
-    { id: 19, imagen: "../assets-bazar/imagenes-mejoradas/iazul2.webp", nombre: "Isotérmico Tropical Blanco – Azul", precio: 2613, },
-    { id: 20, imagen: "../assets-bazar/imagenes-mejoradas/inaranja2.webp", nombre: "Isotérmico Tropical Blanco – Naranja", precio: 2613, },
-    { id: 21, imagen: "../assets-bazar/imagenes-mejoradas/iazuln1.webp", nombre: "Isotérmico Tropical Negro – Azul", precio: 2613, },
-    { id: 22, imagen: "../assets-bazar/imagenes-mejoradas/inaranjan1.webp", nombre: "Isotérmico Tropical Negro – Naranja", precio: 2613, },
-    { id: 23, imagen: "../assets-bazar/imagenes-mejoradas/isoboca1.webp", nombre: "Isotérmico Tropical Boca Jrs", precio: 3065, },
-    { id: 24, imagen: "../assets-bazar/imagenes-mejoradas/isoriver1.webp", nombre: "Isotérmico Tropical River Plate", precio: 3065, },
-];
-
-
-
-
-
-
 
 let contadorCarrito = 0;
 let carrito = [];
+const emptyButton = document.getElementById("buttonCartEmptyView")
+emptyButton.addEventListener("click", () => {
+    carrito.length = 0
+    mostrarCarrito()
+})
 const contadorProductosCart = document.getElementById("cartCount");
 
 const productosHtml = (producto) => {
@@ -52,60 +23,79 @@ const productosHtml = (producto) => {
 }
 
 const productosCart = (producto) => {
-    document.getElementById("modall")
     return `
-    <img src=${producto.imagen}  class="img-width" >    
-    <h5 class="card-title">${producto.nombre}</h5>
-    <p class="card-text">$${producto.precio}</p>
-    <hr></hr>
-    <button id="quitar${producto.idCompra}" class="comprar quit btn-danger">Quitar</button>
-
-    
+    <table class="table table-hover tableCart" id="carritoRender" >         
+    <tbody>           
+                    <td><img src="${producto.imagen}" class="img-cart"</td>
+                    <td class="width-listCart">${producto.nombre}</td>
+                    <td class="width-listCart">$${producto.precio}</td>                    
+                    <td><a href="#" class="eliminarProducto" id="quitar${producto.idCompra}">x</a></td>            
+                </tbody>
+              </table>
+             
 `
 }
-
-
 
 const renderHtml = () => {
     const nodo = document.getElementById("catalogo");
     let catalogoEmpty = "";
-    for (const producto of stock) {
-        catalogoEmpty += productosHtml(producto);
-    }
-    nodo.innerHTML = catalogoEmpty;
-    buttonAdd();
-}
+    fetch('../catalogo.json')
+        .then((response) => response.json())
+        .then((data) => {
 
+            for (const producto of data) {
+                catalogoEmpty += productosHtml(producto);
+            }
+            nodo.innerHTML = catalogoEmpty;
+            console.log(data)
+            buttonAdd();
+            
+        });
+       
+}
 const mostrarCarrito = () => {
-    const nodoCart = document.getElementById("carrito");
+    const nodoCart = document.getElementById("carritoRender");
     let carritoHtml = "";
     for (const producto of carrito) {
         carritoHtml += productosCart(producto);
     }
-
     nodoCart.innerHTML = carritoHtml;
+
     contadorProductosCart.innerText = carrito.length;
     localStorage.setItem(`carrito`, JSON.stringify(carrito));
+
+    const precioTotal = document.getElementById("precioTotal");
+    precioTotal.innerHTML = carrito.reduce((acc, producto) => acc + producto.precio, 0)
     buttonCart()
 }
 
-const buttonAdd = () => {
-    for (const producto of stock) {
-        const buttonId = `agregar${producto.id}`;
-        const nodoButton = document.getElementById(buttonId);
-        nodoButton.addEventListener("click", () => {
 
-            /**  Desestructuración con spread de array */
-            const stock = {
-                ...producto,
-                idCompra: contadorCarrito,
+const buttonAdd = () => {
+    fetch('../catalogo.json')
+        .then((response) => response.json())
+        .then((data) => {
+            for (const producto of data) {
+                const botonId = `agregar${producto.id}`;
+                const botonNodo = document.getElementById(botonId);
+
+                botonNodo.addEventListener("click", () => {
+
+                    const productoCarrito = {
+                        imagen: producto.imagen,
+                        nombre: producto.nombre,
+                        idCompra: contadorCarrito,
+                        precio: producto.precio,
+                    };
+
+                    contadorCarrito += 1;
+                    carrito.push(productoCarrito);
+                    mostrarCarrito();
+                })
             }
-            contadorCarrito += 1;
-            carrito.push(stock);
-            mostrarCarrito();
         })
-    }
-}
+};
+
+
 
 
 const buttonCart = () => {
@@ -113,11 +103,27 @@ const buttonCart = () => {
         const buttonId = `quitar${producto.idCompra}`;
         const buttonNode = document.getElementById(buttonId);
         buttonNode.addEventListener("click", () => {
-            const index = carrito.findIndex((p) => p.idCompra == producto.idCompra);
-            carrito.splice(index, 1);
-            mostrarCarrito();
-
-        });
+            Swal.fire({
+                title: '¿Estás seguro que deseas eliminar?',
+                text: "",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonColor: '#3085d6',
+                cancelButtonColor: '#d33',
+                confirmButtonText: 'Si, quiero borrarlo'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire(
+                        '¡Producto borrado!',
+                        '',
+                        'success'
+                    )
+                    const index = carrito.findIndex((p) => p.idCompra == producto.idCompra);
+                    carrito.splice(index, 1);
+                    mostrarCarrito();
+                }
+            })
+        })
     }
 }
 
@@ -132,27 +138,6 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
-renderHtml();
-
-/*      Es un toast para indicar al usuario que ha agregado un producto al carrito */
-for (const producto of stock) {
-    const btn = document.getElementById(`agregar${producto.id}`)
-    btn.addEventListener("click", () => {
-        Toastify({
-            text: "Producto agregado al carrito",
-            duration: 3000,
-            close: true,
-            gravity: "top",
-            position: "center",
-            stopOnFocus: true,
-            style: {
-                background: "linear-gradient(to right, #00b09b, #96c93d)",
-            },
-            onClick: function () { }
-        }).showToast();
-
-    })
-}
 
 /*              Operador lógico and */
 buttonCarrito = document.getElementById("dropdownMenuButton")
@@ -161,13 +146,8 @@ buttonCarrito.addEventListener("click", () => {
 });
 
 
-const $pokemon = document.querySelector('#pokemon')
-function renderPokemon () {
-        
-}
 
-/*fetch para cargar datos de manero asincrónica */
+renderHtml();
 
- fetch('https://pokeapi.co/api/v2/pokemon/ditto/')
- .then((response) => response.json())
- .then(pokemon => console.log(pokemon) )
+
+
